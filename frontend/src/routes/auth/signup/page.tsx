479a7dfd8ef9312b72351a35da350aba00506eca
Remy349 <santoraga15@gmail.com>
2024-05-17 23:36:36 -0600

    feat: Added responsive design to the home, sign up and sign in pages

--- frontend/src/routes/auth/signup/page.tsx
+++ frontend/src/routes/auth/signup/page.tsx
@@ -2,8 +2,8 @@ import { SignUpForm } from '../_components/signup-form'
 
 export default function SignUpPage() {
   return (
-    <section className='pt-[4rem] pb-[2.5rem]'>
-      <div className='px-6'>
+    <section className='pt-[4rem] pb-[2.5rem] lg:pt-[6rem] lg:pb-[4rem]'>
+      <div className='px-6 sm:mx-auto sm:max-w-lg'>
         <div>
           <SignUpForm />
         </div>
--- frontend/src/routes/landing/_components/navbar.tsx
+++ frontend/src/routes/landing/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <header className='fixed top-0 left-0 bg-white w-full border-b'>
-      <nav className='flex items-center justify-between px-6 h-16'>
+      <nav className='flex items-center justify-between px-6 h-16 sm:mx-auto sm:max-w-2xl lg:max-w-5xl'>
         <Link className='font-bold text-lg' to='/'>
           QuizAPP
         </Link>
