6dcfb948ddb00aa66cd886ee519d480aeff23620
Remy349 <santoraga15@gmail.com>
2024-05-18 18:36:44 -0600

    feat: Add CORS white list

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,22 @@ import { usersRouter } from './routes/users'
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const WHITE_LIST = ['http://localhost:5173']
+
+      if (!origin) return callback(null, true)
+
+      if (WHITE_LIST.includes(origin)) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    }
+  })
+)
+
 app.use(express.json())
 app.use(morgan('dev'))
 
