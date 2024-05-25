8f623c94223a5d3c3e3a8bc53714cacb72d8790b
Remy349 <santoraga15@gmail.com>
2024-05-24 23:26:49 -0600

    feat: New file to handle form validation schemas

---
+++ frontend/src/utils/formSchemas.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+export const SignInFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Email is required' })
+    .email({ message: 'Email is invalid' }),
+  password: z.string().min(1, { message: 'Password is required' }),
+})
+
+export type TSignInFormSchema = z.infer<typeof SignInFormSchema>
+
+// -----------------------------------------------------
+
+export const SignUpFormSchema = z.object({
+  username: z.string().min(1, { message: 'Username is required' }),
+  email: z
+    .string()
+    .min(1, { message: 'Email is required' })
+    .email({ message: 'Email is invalid' }),
+  password: z.string().min(8, { message: 'Password is required' }),
+})
+
+export type TSignUpFormSchema = z.infer<typeof SignUpFormSchema>
