926e5cd2077054caf878d97188adc28f306a93f2
Remy349 <santoraga15@gmail.com>
2024-05-24 23:27:04 -0600

    feat: Added new routes to router config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import LandingRoot from './routes/landing/root'
 import HomePage from './routes/landing/home/page'
 import SignInPage from './routes/auth/signin/page'
 import SignUpPage from './routes/auth/signup/page'
+
+import DashboardRoot from './routes/dashboard/root'
 
 const router = createBrowserRouter([
   {
@@ -19,12 +22,36 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: '/auth/signin',
-        element: <SignInPage />,
+        path: 'auth',
+        children: [
+          {
+            path: 'signin',
+            element: <SignInPage />,
+          },
+          {
+            path: 'signup',
+            element: <SignUpPage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardRoot />,
+    children: [
+      {
+        index: true,
+        element: <h1>HOME STUDENT DASHBOARD</h1>,
       },
       {
-        path: '/auth/signup',
-        element: <SignUpPage />,
+        path: 'admin',
+        children: [
+          {
+            index: true,
+            element: <h1>HOME ADMIN DASHBOARD</h1>,
+          },
+        ],
       },
     ],
   },
