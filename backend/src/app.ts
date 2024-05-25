8bab891de524e07c9554752b929432cbf595c7db
Remy349 <santoraga15@gmail.com>
2024-05-24 23:25:06 -0600

    refactor: CORS options are refactor into a new single file call cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
-import cors from 'cors'
+
+import cors from './lib/cors'
 
 import { authRouter } from './routes/auth'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const WHITE_LIST = ['http://localhost:5173']
-
-      if (!origin) return callback(null, true)
-
-      if (WHITE_LIST.includes(origin)) {
-        return callback(null, true)
-      }
-
-      return callback(new Error('Not allowed by CORS'))
-    }
-  })
-)
-
+app.use(cors)
 app.use(express.json())
 app.use(morgan('dev'))
 
