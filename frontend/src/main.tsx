63280ec5b3b85d6a56efa2cc9b22c8c7ada441f1
Remy349 <santoraga15@gmail.com>
2024-05-25 21:56:45 -0600

    refactor: Move routers from main.tsx to a single file just for routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,59 +3,9 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
-import LandingRoot from './routes/landing/root'
-import HomePage from './routes/landing/home/page'
-import SignInPage from './routes/auth/signin/page'
-import SignUpPage from './routes/auth/signup/page'
-
-import DashboardRoot from './routes/dashboard/root'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LandingRoot />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'auth',
-        children: [
-          {
-            path: 'signin',
-            element: <SignInPage />,
-          },
-          {
-            path: 'signup',
-            element: <SignUpPage />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: '/dashboard',
-    element: <DashboardRoot />,
-    children: [
-      {
-        index: true,
-        element: <h1>HOME STUDENT DASHBOARD</h1>,
-      },
-      {
-        path: 'admin',
-        children: [
-          {
-            index: true,
-            element: <h1>HOME ADMIN DASHBOARD</h1>,
-          },
-        ],
-      },
-    ],
-  },
-])
+import { router } from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
