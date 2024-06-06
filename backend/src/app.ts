fb524238a24d5462df9792bf51d304dcf40ea5dc
Remy349 <santoraga15@gmail.com>
2024-06-06 00:41:28 -0600

    feat: Added auth route to the api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan'
 
 import cors from './libs/cors'
 
+import authRouter from './auth/infrastructure/api/route/AuthRoute'
 import userRoute from './users/infrastructure/api/route/UserRoute'
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors)
 app.use(express.json())
 app.use(morgan('dev'))
 
+app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRoute)
 
 export default app
