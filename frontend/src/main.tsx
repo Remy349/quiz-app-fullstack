4415e995244004e863f4b28fdf58d71d9bd7f845
Remy349 <santoraga15@gmail.com>
2024-06-07 18:58:34 -0600

    feat: Added auth provider to handle global state of user session

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { RouterProvider } from 'react-router-dom'
+import { AuthContextProvider } from './context/auth/authContextProvider'
 
 import { router } from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
