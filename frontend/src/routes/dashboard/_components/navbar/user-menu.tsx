7c0e2e82ce3c1e91a07fac79c7c2d05db3a957dc
Remy349 <santoraga15@gmail.com>
2024-06-15 17:14:36 -0600

    feat: Loading state component added (Skeleton)

--- frontend/src/routes/dashboard/_components/navbar/user-menu.tsx
+++ frontend/src/routes/dashboard/_components/navbar/user-menu.tsx
@@ -9,8 +9,9 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Avatar } from '@/components/custom/avatar'
 import { LogOut } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
 import { useAuthContext } from '@/hooks/useAuthContext'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useNavigate } from 'react-router-dom'
 
 export const UserMenu = () => {
   const navigate = useNavigate()
@@ -22,7 +23,7 @@ export const UserMenu = () => {
     navigate('/auth/signin')
   }
 
-  if (!currentUser) return <p>Loading...</p>
+  if (!currentUser) return <Skeleton className='w-10 h-10 rounded-full' />
 
   return (
     <DropdownMenu>
@@ -30,7 +31,12 @@ export const UserMenu = () => {
         <Avatar value={currentUser.email} size={42} />
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          My Account
+          <p className='font-normal text-xs text-muted-foreground'>
+            {currentUser.email}
+          </p>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem className='md:cursor-pointer' onClick={handleLogout}>
           Logout
