dbf9e8e351b09cbc4f5c9c6f26ec14abfe4ffb92
Remy349 <santoraga15@gmail.com>
2024-06-15 17:03:19 -0600

    feat: Added button to redirect to dashboard if users is logged in

--- frontend/src/routes/landing/_components/navbar/navbar.tsx
+++ frontend/src/routes/landing/_components/navbar/navbar.tsx
@@ -1,7 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { useAuthContext } from '@/hooks/useAuthContext'
+import { LogIn } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+  const { currentUser } = useAuthContext()
+
   return (
     <header className='fixed top-0 left-0 bg-white w-full border-b z-50'>
       <nav className='flex items-center justify-between px-6 h-16 sm:mx-auto sm:max-w-2xl lg:max-w-5xl'>
@@ -9,9 +13,18 @@ export const Navbar = () => {
           QuizAPP
         </Link>
         <div>
-          <Button className='font-medium' variant='outline' asChild>
-            <Link to='/auth/signin'>Sign In</Link>
-          </Button>
+          {currentUser ? (
+            <Button className='font-medium' variant='outline' asChild>
+              <Link to='/dashboard'>
+                Dashboard
+                <LogIn className='ml-2 w-5 h-5' />
+              </Link>
+            </Button>
+          ) : (
+            <Button className='font-medium' variant='outline' asChild>
+              <Link to='/auth/signin'>Sign In</Link>
+            </Button>
+          )}
         </div>
       </nav>
     </header>
