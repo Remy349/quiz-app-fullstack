3374265f3065bebf7beeb5f53913421eafe5ccb3
Remy349 <santoraga15@gmail.com>
2024-06-21 17:53:47 -0600

    feat: Added query client provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,19 @@
 import './index.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthContextProvider } from './context/auth/authContextProvider'
-
 import { router } from './routes/routes'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
