27ac2714472ccbeed12855436bc62c9959208d00
Remy349 <santoraga15@gmail.com>
2024-06-21 17:54:14 -0600

    feat: Added services folder. API - Mutations - Queries.

---
+++ frontend/src/services/api/categories.ts
@@ -0,0 +1,13 @@
+import { TCategory } from '@/types/types'
+import { API_URL } from '@/utils/consts'
+import axios from 'axios'
+
+export const getCategories = async () => {
+  const res = await axios.get<TCategory[]>(`${API_URL}/categories`)
+
+  return res.data
+}
+
+export const createCategory = async (category: { name: string }) => {
+  await axios.post(`${API_URL}/categories`, category)
+}
