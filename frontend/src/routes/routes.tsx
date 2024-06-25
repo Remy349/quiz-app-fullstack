5f9ea3cb14ee8bda07bcb2bbbf61da5be987a98d
Remy349 <santoraga15@gmail.com>
2024-06-24 20:19:19 -0600

    refactor: Modify paths structure for user dashboard and handle quizzes

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import LandingRoot from './landing/root'
+
 import HomePage from './landing/home/page'
 import SignInPage from './auth/signin/page'
 import SignUpPage from './auth/signup/page'
 
 import DashboardRoot from './dashboard/root'
+
 import DashboardUserHomePage from './dashboard/user/page'
+import DashboardUserQuizzesPage from './dashboard/user/quizzes/page'
+import DashboardUserQuizzesNewPage from './dashboard/user/quizzes/new/page'
 
 import DashboardAdminHomePage from './dashboard/admin/page'
 import DashboardAdminCategoriesPage from './dashboard/admin/categories/page'
@@ -42,6 +46,19 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <DashboardUserHomePage />,
+      },
+      {
+        path: 'quizzes',
+        children: [
+          {
+            index: true,
+            element: <DashboardUserQuizzesPage />,
+          },
+          {
+            path: 'new',
+            element: <DashboardUserQuizzesNewPage />,
+          },
+        ],
       },
       {
         path: 'admin',
