a9d19a4b7e72bb1bed771019245469bf29cade18
Remy349 <santoraga15@gmail.com>
2024-06-28 13:34:47 -0600

    feat: Added new api endpoint route for quizzes.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import cors from './libs/cors'
 import authRouter from './auth/infrastructure/api/route/AuthRoute'
 import userRouter from './users/infrastructure/api/route/UserRoute'
 import categoryRouter from './categories/infrastructure/api/route/CategoryRoute'
+import quizRouter from './quizzes/infrastructure/api/route/QuizRoute'
 
 const app = express()
 
@@ -16,5 +17,6 @@ app.use(morgan('dev'))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/quizzes', quizRouter)
 
 export default app
